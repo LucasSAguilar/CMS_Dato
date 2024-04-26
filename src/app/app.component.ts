d57089c05cd67cb0eafaaef3975c8bc073f479a0
Lucas Aguilar <lucassaguilar01@gmail.com>
2024-04-26 11:22:25 -0300

    Organização e solicitação de imagens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { NgFor } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DatoConnectService } from './services/dato-connect.service';
 
 @Component({
   selector: 'app-root',
@@ -17,40 +18,23 @@ export class AppComponent {
   titulos = [];
   imagens = [];
 
-  constructor(private http: HttpClient) {
-    this.fazerColeta();
+  constructor(
+    private http: HttpClient,
+    private datoConnect: DatoConnectService
+  ) {
+    this.coletarTitulos();
+    this.coletarImagens();
   }
 
-  getProducts() {
-    return this.http.get<any>(this.apiUrl, {
-      headers: {
-        Authorization: `Bearer ${this.authToken}`,
-      },
-      params: {
-        query: `
-        {
-          allTitulos {
-            id
-            nome
-          }
-        }
-        `,
-      },
-    });
-  }
-  // ----------------------------
-  fazerColeta() {
-    this.getProducts().subscribe((response) => {
-      console.log(response.data.allTitulos);
-      this.titulos = response.data.allTitulos.map(
-        (titulo: any) => titulo.nome
-      );
+  coletarTitulos() {
+    this.datoConnect.getTitulos(this.http).subscribe((data) => {
+      this.titulos = data.data.allTitulos.map((titulo: any) => titulo.nome);
     });
   }
 
-  exibirMensagem() {
-    setTimeout(() => {
-      console.log(this.titulos);
-    }, 2000);
+  coletarImagens() {
+    this.datoConnect.getImagens(this.http).subscribe((data) => {
+      this.imagens = data.data.allImagems.map((imagem: any) => imagem.link);
+    });
   }
 }
--- src/app/services/dato-connect.service.ts
+++ src/app/services/dato-connect.service.ts
@@ -1,9 +1,47 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DatoConnectService {
+  private apiUrl = 'https://graphql.datocms.com/';
+  private authToken: String = 'bec83a609f470a13b042611bbd1c77';
 
-  constructor() { }
+  constructor() {}
+
+  getTitulos(http: HttpClient) {
+    return http.get<any>(this.apiUrl, {
+      headers: {
+        Authorization: `Bearer ${this.authToken}`,
+      },
+      params: {
+        query: `
+        {
+          allTitulos {
+            id
+            nome
+          }
+        }
+        `,
+      },
+    });
+  }
+
+  getImagens(http: HttpClient) {
+    return http.get<any>(this.apiUrl, {
+      headers: {
+        Authorization: `Bearer ${this.authToken}`,
+      },
+      params: {
+        query: `
+        {
+          allImagems {
+            link
+          }
+        }
+        `,
+      },
+    });
+  }
 }
